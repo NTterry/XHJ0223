87b376a3c0cd434e397ab45b2c0e25f32c6f8026
Terry <CTH0518@yeah.net>
2020-03-01 18:08:03 +0800

    Update Encoder.h
    Build this module

--- Src/Encoder/Encoder.h
+++ Src/Encoder/Encoder.h
@@ -1,5 +1,19 @@
-
-
+/**********************************************************************************
+  Copyright (C),2018-2020, JSJJ Tech.Co., Ltd
+  FileName: Encoder.h
+  Author:   Terryc     Version:V1.0    Data:2020.3.1
+  Description: 增量编码器测量长度，速度和加速度的算法和应用
+  Others: None
+  Function List:
+      void Enc_Clr_TotalCnt1(void);
+      void Enc_Clr_TotalCnt2(void); 
+      int32_t Enc_Get_Acce(void);
+      int32_t Enc_Get_CNT1(void);
+      int32_t Enc_Get_CNT2(void);
+      void Enc_Set_Dir(int dir);
+  History: 
+      Terryc  V1.0   2020.3.1    build this module
+***********************************************************************************/
 #ifndef _ENCODER_H_
 #define _ENCODER_H_
 #include "stdint.h"
@@ -7,7 +21,7 @@
 #define MAXCOUNT            30000                   //最大计数值
 #define ENCODER_TIM_PERIOD  (MAXCOUNT * 2)          //自动重装值
 #define SPTIMEOUT           300                     // 速度检测超时时间  300ms
-#define CNT_DIV     		4     //TIM_ENCODERMODE_TI12 模式下实际结果需要4分频
+#define CNT_DIV     		    4                       //TIM_ENCODERMODE_TI12 模式下实际结果需要4分频
 
 extern void HwEcInit(void);
 extern void HwEcExitCallBack(void);			// configuration a encoder's pin with Exti Interrupt for this fun
@@ -17,7 +31,6 @@ extern void HwEcTimerTick1ms(void);			//  call by 1ms Tick fun
 /*接口函数*/
 void Enc_Clr_TotalCnt1(void); 
 void Enc_Clr_TotalCnt2(void); 
-int32_t Enc_Get_SpeedE1(void);
 int32_t Enc_Get_Acce(void);
 int32_t Enc_Get_CNT1(void);
 int32_t Enc_Get_CNT2(void);
